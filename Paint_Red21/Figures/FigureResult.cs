d42e79de71e7b7e942517927042a734bcb6a1b0a
Nerom <graver9062@tutanota.de>
2020-11-29 19:10:04 +0300

    Added class FigureResult
    This class return location of mouse and image for bitmap

---
+++ Paint_Red21/Figures/FigureResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace Paint_Red21.Figures
+{
+    public class FigureResult
+    {
+        public Bitmap Image { get; }
+        public Point Location { get; }
+        public FigureResult(Bitmap image, Point location)
+        {
+            Image = image;
+            Location = location;
+        }
+    }
+}
